56862dc02d356b909718b3a71ba298d774c9c7a6
Jenkan27 <holmjens84@gmail.com>
2024-01-17 10:53:30 +0100

    de sant

--- app.py
+++ app.py
@@ -6,9 +6,17 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/buildpage')
-def buildpage():
-    return render_template('buildpage.html')
+@app.route('/buildpagesvart')
+def buildpagesvart():
+    return render_template('buildpagesvart.html')
+
+@app.route('/buildpagevit')
+def buildpagevit():
+    return render_template('buildpagevit.html')
+
+@app.route('/buildpagebla')
+def buildpagebla():
+    return render_template('buildpagebla.html')
 
 if  __name__ == '__main__':
     app.run(debug=True)
